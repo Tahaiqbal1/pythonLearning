4921ec74e2d1cccfbd026a93285b241cb170ae5b
Taha Iqbal <tahaiqbal001@gmail.com>
2023-09-16 16:02:40 +0500

    Learning Pyhton

---
+++ helloWorld.py
@@ -0,0 +1,20 @@
+# firstNumber = input('First: ')
+# secondNumber = input('Second: ')
+# sum = float(firstNumber) + float(secondNumber)
+# print(sum)
+
+
+# Weight Converter
+# weight = float(input('Weight: '))
+# unit= input("(K)g or (L)b: ")
+
+# weightType = unit.casefold()
+
+# weightInKg = (weight * 2.20462)
+# weightInLb = (weight * 0.453592)
+# if weightType == 'k':
+#   print(f"weight in Kg: {weightInKg:.2f}")
+# else:
+#   print(f"weight in pound : {weightInLb:.2f}")
+
+
